86dc69321b973bf844ae46a15249e7f92a9b2a02
NavPilDev <abhinavpillai04@gmail.com>
2025-02-15 16:20:10 -0600

    init

---
+++ app/api/openai/route.ts
@@ -0,0 +1,35 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+// Create an OpenAI API client
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "",
+});
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+export async function POST(req: Request, res: Response) {
+  // Extract the `prompt` from the body of the request
+  const { messages } = await req.json();
+  console.log("messages:", messages);
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Create a flight path.",
+      },
+      ...messages,
+    ],
+    stream: true,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
